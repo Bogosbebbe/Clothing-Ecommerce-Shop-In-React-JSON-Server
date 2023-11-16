4fb39d91db68ab6788adcd68dfa4ca389ba463a9
Kuzma02 <aleksandarkuzmanovic02@gmail.com>
2023-11-16 10:20:29 +0100

    “created_basic_functionality_for_storing_in_order_history”

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
 );
--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -1,27 +1,58 @@
 import React from "react";
 import { SectionTitle } from "../components";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 const ThankYou = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+  console.log(cartItems);
+
+  const saveToOrderHistory = async () => {
+    try {
+      const response = await axios.post("http://localhost:8080/orders", {userId: localStorage.getItem("id"), cartItems: cartItems});
+    } catch (err) {
+      console.log(err.response);
+    }
+  };
+
+  if (cartItems.length > 0) {
+    saveToOrderHistory();
+  }
+
   return (
     <>
       <SectionTitle title="Thank You" path="Home | Cart | Thank you" />
       <div className="thankyou-content text-center text-accent-content px-10 max-w-7xl mx-auto">
-        <h2 className="text-6xl max-sm:text-4xl">Thank you for your purchase!</h2>
+        <h2 className="text-6xl max-sm:text-4xl">
+          Thank you for your purchase!
+        </h2>
 
         <h3 className="text-2xl mt-10 max-sm:text-xl">
           We hope you love your new clothes and shoes! We appreciate your
           business and look forward to seeing you again soon.
         </h3>
-        <h3 className="text-2xl mt-5 max-sm:text-xl">Here are some things you can do next:</h3>
+        <h3 className="text-2xl mt-5 max-sm:text-xl">
+          Here are some things you can do next:
+        </h3>
         <ul className="text-xl mt-5 text-blue-500 max-sm:text-lg">
-          <li className="hover:text-blue-600 cursor-pointer"><Link to="/order-history">&rarr; See order history &larr;</Link></li>
-          <li className="hover:text-blue-600 cursor-pointer"><Link to="/">&rarr; Browse more product and buy more &larr;</Link></li>
-          <li className="hover:text-blue-600 cursor-pointer">&rarr; Follow us on social media &larr;</li>
+          <li className="hover:text-blue-600 cursor-pointer">
+            <Link to="/order-history">&rarr; See order history &larr;</Link>
+          </li>
+          <li className="hover:text-blue-600 cursor-pointer">
+            <Link to="/">&rarr; Browse more product and buy more &larr;</Link>
+          </li>
+          <li className="hover:text-blue-600 cursor-pointer">
+            &rarr; Follow us on social media &larr;
+          </li>
         </ul>
 
-        <h4 className="text-xl mt-5 max-sm:text-lg">Thank you again for your purchase!</h4>
-        <h4 className="text-xl max-sm:text-lg">Sincerely, The Kuzma Clothing & Shoes team</h4>
+        <h4 className="text-xl mt-5 max-sm:text-lg">
+          Thank you again for your purchase!
+        </h4>
+        <h4 className="text-xl max-sm:text-lg">
+          Sincerely, The Kuzma Clothing & Shoes team
+        </h4>
       </div>
     </>
   );
